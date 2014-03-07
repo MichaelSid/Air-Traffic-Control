f8577f8acb10c31f52cad4d13524a88bc1996331
Michael <michael.sidon@gmail.com>
2014-03-07 16:14:58 +0000

    Added flying and landing methods to Plane class

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,4 +1,20 @@
 class Plane
+
+	def initialize
+		fly
+	end 
+
 	def landed?
+		@landed
 	end
+
+	def land
+		@landed = true
+	end
+
+	def fly
+		@landed = false
+	end
+
+
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -7,5 +7,18 @@ describe Plane do
 		flying_plane = Plane.new
 		expect(flying_plane.landed?).to be_false
 	end
-	
+
+	it "should be able to land" do
+		flying_plane = Plane.new
+		flying_plane.land
+		expect(flying_plane.landed?).to be_true
+	end
+
+	it "should be able to able to fly when it's landed" do
+		flying_plane = Plane.new
+		flying_plane.land
+		flying_plane.fly
+		expect(flying_plane.landed?).to be_false
+	end
+
 end
